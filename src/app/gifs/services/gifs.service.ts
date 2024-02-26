d5a3ad3db0ff09b0c769524f65939743b640a588
Franco Amon Maldonado <franco.m_887@outlook.com>
2024-02-26 02:55:32 -0300

    Implemented service gifsService ngFor in button sideBar

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private _tagsHistory: string[]= []
+
+
+
+
+  constructor() {
+
+   }
+
+
+  get tagsHistory(){
+    return [...this._tagsHistory]
+  }
+
+  searchTag(tag:string):void{
+    if(tag === "") return
+
+    this._tagsHistory.unshift(tag)
+  }
+}
