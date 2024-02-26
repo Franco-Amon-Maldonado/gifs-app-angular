a3acb0ef0ee2a127f790127bf573e35473130f2f
Franco Amon Maldonado <franco.m_887@outlook.com>
2024-02-26 04:17:19 -0300

    Added environment Implemented httpClient

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,33 +1,44 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
   private _tagsHistory: string[] = [];
+  private apiKey: string = environment.apiKey;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
   private organizeHistory(tag: string) {
-    tag = tag.toLowerCase()
+    tag = tag.toLowerCase();
 
-  if(this._tagsHistory.includes(tag)){
-    //Filtro para no dejar pasar el mismo tag que ya se introdujo
-    this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag)
+    if (this._tagsHistory.includes(tag)) {
+      //Filtro para no dejar pasar el mismo tag que ya se introdujo
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+
+    this._tagsHistory.unshift(tag);
+    //Limito que sea a 10 busquedas
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
   }
 
-  this._tagsHistory.unshift(tag)
-  //Limito que sea a 10 busquedas
-  this._tagsHistory = this._tagsHistory.splice(0, 10)
-
-  }
-
-  searchTag(tag: string): void {
+  searchTag(tag: string) {
     if (tag === '') return;
-    this.organizeHistory(tag)
+    this.organizeHistory(tag);
+
+    //  const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${tag}&limit=10`)
+
+    this.http.get(
+      `https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${tag}&limit=10`)
+      .subscribe( resp => {
+        console.log(resp);
+        
+      })
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    apiKey: "t2bymsmmAm1bPfSEPQvt9fWIIDS1po4r"
+};
