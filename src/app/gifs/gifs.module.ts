c594c66555d73c4f94e13cc4140a190ce8c0b8b9
Franco Amon Maldonado <franco.m_887@outlook.com>
2024-02-25 23:45:34 -0300

    Import/exports components

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './pages/home/home.component';
+import {HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomePageComponent,
+    SearchBoxComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomePageComponent
   ]
 })
 export class GifsModule { }
