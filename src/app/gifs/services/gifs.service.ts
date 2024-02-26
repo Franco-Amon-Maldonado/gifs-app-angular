d24bae21749060eccd7c4479ee855b5778c33f67
Franco Amon Maldonado <franco.m_887@outlook.com>
2024-02-26 03:23:43 -0300

    Function for organized the search history

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,27 +1,33 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GifsService {
+  private _tagsHistory: string[] = [];
 
-  private _tagsHistory: string[]= []
+  constructor() {}
 
-
-
-
-  constructor() {
-
-   }
-
-
-  get tagsHistory(){
-    return [...this._tagsHistory]
+  get tagsHistory() {
+    return [...this._tagsHistory];
   }
 
-  searchTag(tag:string):void{
-    if(tag === "") return
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase()
 
-    this._tagsHistory.unshift(tag)
+  if(this._tagsHistory.includes(tag)){
+    //Filtro para no dejar pasar el mismo tag que ya se introdujo
+    this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag)
+  }
+
+  this._tagsHistory.unshift(tag)
+  //Limito que sea a 10 busquedas
+  this._tagsHistory = this._tagsHistory.splice(0, 10)
+
+  }
+
+  searchTag(tag: string): void {
+    if (tag === '') return;
+    this.organizeHistory(tag)
   }
 }
