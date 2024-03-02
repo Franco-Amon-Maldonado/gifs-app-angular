e314c50b122541ca0cd0f7f8f4b1df3b1e935d3f
Franco Amon Maldonado <franco.m_887@outlook.com>
2024-03-02 04:47:43 -0300

    Finish lazy image

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = "";
+
+  public loading: boolean = false;
+
+
+  ngOnInit(): void {
+    if (!this.url) {
+      throw new Error('la url es requerida');
+    }
+  }
+
+  onLoad(): void {
+    this.loading = true;
+  }
+
 
 }
