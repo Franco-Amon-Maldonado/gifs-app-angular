0e92c8614ef3f8e1650c88160fec41191e750a76
Franco Amon Maldonado <franco.m_887@outlook.com>
2024-03-02 05:02:43 -0300

    Animation fadein for images

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
+  styleUrl: './lazy-image.component.css'
 })
 export class LazyImageComponent implements OnInit {
   @Input()
